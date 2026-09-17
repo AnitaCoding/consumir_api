1a727e60b8d972c70821f234d776119629b74944
AnitaCoding <ana.amago.aparicio@gmail.com>
2026-09-17 22:17:45 +0200

    Primer commit

--- main.py
+++ main.py
@@ -28,5 +28,5 @@ response = consulta.get(f'https://api.chucknorris.io/jokes/random?category={cate
 #print('Código HTTP de respuesta: ', response.status_code)
 #print('Cabecera: ', response.headers['content-type'])
 #print('Encoding: ', response.encoding)
-#print('Respuesta en string: ', response.text)
+print('Respuesta en string: ', response.text)
 print('Respuesta en JSON', response.json())
