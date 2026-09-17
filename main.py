c9f7211723b24ceec96accfc988d0a7000aa9e13
AnitaCoding <ana.amago.aparicio@gmail.com>
2026-09-18 01:57:26 +0200

    Añado ejemplo para obtener un valor concreto del diccionario

--- main.py
+++ main.py
@@ -28,5 +28,8 @@ response = consulta.get(f'https://api.chucknorris.io/jokes/random?category={cate
 #print('Código HTTP de respuesta: ', response.status_code)
 #print('Cabecera: ', response.headers['content-type'])
 #print('Encoding: ', response.encoding)
-print('Respuesta en string: ', response.text)
+#print('Respuesta en string: ', response.text)
 print('Respuesta en JSON', response.json())
+
+#Ejemplo para acceder a un valor del JSON
+print(response.json()['value'])
